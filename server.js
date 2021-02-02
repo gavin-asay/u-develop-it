05486315d18dc989c6a29583f803a2fd0fabdea5
Gavin Asay <gavin.asay@gmail.com>
2021-02-02 03:44:31 -0700

    add post route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -59,6 +60,34 @@ app.delete('/api/candidate/:id', (req, res) => {
 		res.json({
 			message: 'successfully deleted',
 			changes: this.changes,
+		});
+	});
+});
+
+app.post('/api/candidate', ({ body }, res) => {
+	const errors = inputCheck(
+		body,
+		'first_name',
+		'last_name',
+		'industry_connected'
+	);
+	if (errors) {
+		res.status(400).json({ error: errors });
+	}
+
+	const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) VALUES (?,?,?)`;
+	const params = [body.first_name, body.last_name, body.industry_connected];
+
+	db.run(sql, params, function (err, result) {
+		if (err) {
+			res.status(400).json({ error: err.message });
+			return;
+		}
+
+		res.json({
+			message: 'success',
+			data: body,
+			id: this.lastID,
 		});
 	});
 });
